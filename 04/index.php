840a0a8056e8bf849bc83e883d7f2824a5d4e6a3
Ruslan <veb3solidity@gmail.com>
2024-02-28 13:41:27 +0200

    less4

--- 04/index.php
+++ 04/index.php
@@ -1,15 +1,17 @@
 
 <?php
 error_reporting(0); // настройка вывода ошибок, 0 - не выводить ошибки, -1 - выводить все ошибки
+
 $title = 'Мой заголовок';
 $winnie_puh = "Hello, I'm Vinnie";
 $winnie_puh2 = 'Hello, I\'m Vinnie2';   # \ -- экранируем спец символы
+// если ненадо экранировать, а мы экранируем - обратный слэш выводится
 $fruit = 'orange';
 $from_winnie = "I have $fruit"; # в 2-х кавычках переменные обрабатываются
 $from_winnie2 = "I have 2 {$fruit}s"; # явно указаваем где переменная
 // ПЕРЕМЕННЫЕ ЧУСТВИТЕЛЬНЫ К РЕГИСТРУ
 $var = 'Gosha';
-//$Var = 'Gosha1';
+$Var = 'Gosha1';
 ?>
 <!doctype html>
 <html lang="en">
